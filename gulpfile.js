9740f642599146ec7fa5186ccd9058999152d31b
conggao <578422314@qq.com>
2017-01-30 13:30:14 +0000

    容器创建后失败运行

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // 载入外挂
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-scss'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,5 @@
 export App from './App'
 export Home from './Home'
 export Foo from './Foo'
-export Bar from './Bar'
+export JsonList from './JsonList'
 export Antd from './Antd'
-export LocalBar from './LocalBar'
