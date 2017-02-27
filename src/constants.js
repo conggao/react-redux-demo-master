8bc7bb8efce55bb1e402db0ccd6706ea465493c5
conggao <578422314@qq.com>
2017-02-27 00:01:10 +0000

    修正架包

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,7 @@
+/*收藏json代码片段*/
 export const ADDJSON = 'ADDJSON';
 export const GETJSONLIST ='GETJSONLIST';
 export const GETFORMDATA="GETFROMDATA";
 export const CHANGEFORMSTATE="CHANGEFORMSTATE";
+/*构造form表单*/
+export const GETFORMMATE = "GETFORMMATE";
